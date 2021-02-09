6cb7ef415addaf912a041c68933c00578fa7a1b9
sagar-code <sagarfmt@gmail.com>
2021-02-09 11:42:44 +0545

    insight workshop

---
+++ assignment/DataType-and-Functions/Data-Types/1.py
@@ -0,0 +1,4 @@
+# 1. Write a Python program to count the number of characters (Character frequency) in a (string).
+# Sample String: google.com
+# Excepted Result: {'o': 3, 'g': 2, '.': 1, 'e': 1, 'l': 1, 'm': 1, 'c': 1}
+
