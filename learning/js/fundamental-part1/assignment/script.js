f58554310d23d856d04affb3455ad0a6198f4e13
sagar-code <sagarfmt@gmail.com>
2021-03-28 17:12:28 +0545

    learning js

---
+++ learning/js/fundamental-part1/assignment/script.js
@@ -0,0 +1,44 @@
+// Values and Variaables
+country = "Nepal";
+continent = "Asia";
+population = 300;
+
+console.log(`Country: ${country}`);
+console.log(`Continent: ${continent}`);
+console.log(`Population: ${population}`);
+
+// Data Types
+isIsland = false;
+let language;
+
+console.log("typeof isIsland:", typeof isIsland);
+console.log("typeof population:", typeof population);
+console.log("typeof country:", typeof country);
+console.log("typeof language:", typeof language);
+
+// let, const and var
+language = "Nepali";
+const locatedIn = "Asia";
+console.log("located in : ", locatedIn);
+
+// Basic Operators
+
+day = "tuesday";
+switch (day) {
+  case "sunday":
+    console.log("today is sunday");
+    console.log("today is sunday");
+    console.log("today is sunday");
+    break;
+  case "monday":
+    console.log("Monday");
+    console.log("Monday");
+    console.log("Monday");
+    break;
+  case "tuesday":
+  case "wednesday":
+    console.log("WEdnesday");
+    break;
+  default:
+    console.log("enter valid day");
+}
